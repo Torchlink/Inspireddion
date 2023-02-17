acd351ea76c53045275ac7823ef7a3f5fd7fe88b
MStrauch <marcus.strauch2@gmail.com>
2023-02-17 18:03:30 +0100

    Small accessibility update

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -14,13 +14,21 @@ export const Sidebar = () => {
   const options = useSelector(selectAllOptions);
   const filteredSubreddits = useSelector(selectFilteredSubreddits);
   const loadedSubreddits = useSelector(selectSubreddits);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const subreddits = filteredSubreddits ? filteredSubreddits : loadedSubreddits;
 
   return (
     <div className={options.sidebarOpen ? "sidebar open" : "sidebar"}>
-      <input className="sidebarInput" type="text" onChange={() => {dispatch(filterSubreddits(filterSubredditRef.current.value))}} ref={filterSubredditRef} />
+      <input
+        className="sidebarInput"
+        type="text"
+        placeholder="Filter subreddits"
+        onChange={() => {
+          dispatch(filterSubreddits(filterSubredditRef.current.value));
+        }}
+        ref={filterSubredditRef}
+      />
       <SubredditList subreddits={subreddits} />
     </div>
   );
